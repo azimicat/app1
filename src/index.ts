6d0eac3aa8c2daa71bf7f8f1d702043d49850ec1
azimicat <0.0.nn.ai@gmail.com>
2021-10-05 14:52:44 +0900

    Update index.ts
    [openBD](https://openbd.jp/#contact)から情報を取得する処理を追加

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
+const request = require("request");
 
+// [openBD](https://openbd.jp/#contact)
+var url = "https://api.openbd.jp/v1/get",
+    isbn = "978-4-12-102408-4,978-4-7808-0204-7,978-4-908875-40-3",
+    option = "pretty";
+const requestUrl = url.concat("?isbn=", isbn, "&", option);
+
+// リクエストURLの確認
+console.log(requestUrl);
+
+request(requestUrl, function (error, response, data) {
+  if (error != null) {
+    console.error("error:", error);
+  }
+  console.log("statusCode:", response && response.statusCode);
+  const parsedData = JSON.parse(data);
+  // console.log(parsedData); // 取得データの確認
+  for(var i = 0; i<parsedData.length; i++) {
+    // isbn1件ごとのデータを出力
+    console.log(parsedData[i].summary);
+  }
+});
